6bcfaab979b46a02369f20c55ee9b2afdfa1f3d0
Henry Arze <hankatari@comcast.net>
2026-07-10 14:00:42 -0400

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        
+        # FIXED WINDOW SIZE
+        
+        d = {}
+        # get s1 dict w/ freq.
+        for s in s1:
+            d[s] = d.get(s, 0) + 1
+
+        l = 0
+        s1_length = len(s1)
+        d2 = {}
+        for x in range(len(s2)):
+            # update dict2 for s2
+            d2[s2[x]] = d2.get(s2[x], 0) + 1 
+            # reached correct window size, check if 2 dicts are equal
+            if(x-l + 1 == s1_length and d2 == d):
+                return True
+            # move left pointer and update dict2 when reached max window size
+            elif(x-l + 1 == s1_length):
+                d2[s2[l]] = d2.get(s2[l], 0) - 1
+                d2 = {key: val for key, val in d2.items() if val != 0} # remove key if freq. = 0
+                l = l + 1
+
+        return False
+            
+        
